dc2bc44483d8dc3b01391697bf5f7eb893a90a63
kyodkyo <dkekry@naver.com>
2022-06-06 15:29:49 +0900

    update

---
+++ review/src/Num04.java
@@ -0,0 +1,14 @@
+public class Num04 {
+    public static void main(String[] args) {
+        int output = fibonacci(10);
+        System.out.println(output);
+    }
+
+    public static int fibonacci(int n){
+        if(n<2){
+            return n;
+        }
+
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
---
+++ review/src/Num06.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Num06 {
+    public static void main(String[] args) {
+        int output = multipleOfArray(new int[]{-1, 4, 2, -7});
+        System.out.println(output);
+    }
+
+    public static int multipleOfArray(int[] arr){
+        if(arr.length==1){
+            return arr[0];
+        }
+
+        int head = arr[0];
+        int[] tail = Arrays.copyOfRange(arr, 1, arr.length);
+
+        return head * multipleOfArray(tail);
+    }
+}
