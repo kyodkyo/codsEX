9271712e181c3857e56d3d3e5139d340f1128d5d
kyodkyo <dkekry@naver.com>
2022-06-08 15:56:52 +0900

    update

--- codsDaily/src/daily10.java
+++ codsDaily/src/daily10.java
@@ -1,5 +1,29 @@
 public class daily10 {
     public static void main(String[] args) {
-        
+        String output = insertDash("454793");
+        System.out.println(output); // --> 4547-9-3
+
+        output = insertDash("6734611997679419173");
+        System.out.println(output);
+    }
+
+    public static String insertDash(String str) {
+        if(str.length()==0){
+            return null;
+        }
+
+        String result = String.valueOf(str.charAt(0));
+
+        for(int i=1; i<str.length(); i++){
+            int num1 = str.charAt(i-1);
+            int num2 = str.charAt(i);
+
+            if(num1%2==1 && num2%2==1){
+                result += "-";
+            }
+            result += str.charAt(i);
+        }
+
+        return result;
     }
 }
