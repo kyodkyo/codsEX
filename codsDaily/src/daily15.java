2832b9c1b07f86444da3ed261f58e4dfce6ae0bb
kyodkyo <dkekry@naver.com>
2022-06-15 15:18:06 +0900

    update

---
+++ codsDaily/src/daily15.java
@@ -0,0 +1,21 @@
+public class daily15 {
+    public static void main(String[] args) {
+        Integer output = modulo(25, 4);
+        System.out.println(output); // --> 1
+
+        output = modulo(25, 0);
+        System.out.println(output); // --> null
+    }
+
+    public static Integer modulo(int num1, int num2) {
+        if(num2==0){
+            return null;
+        }
+
+        while(num1>=num2){
+            num1 -= num2;
+        }
+
+        return num1;
+    }
+}
