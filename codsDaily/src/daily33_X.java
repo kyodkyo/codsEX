2a8705a47a9d3e483580ef7cdcfce9a7f8f2baa1
kyodkyo <dkekry@naver.com>
2022-07-13 15:56:03 +0900

    update

---
+++ codsDaily/src/daily33_X.java
@@ -0,0 +1,39 @@
+public class daily33_X {
+    public static void main(String[] args) {
+        int[][] room = new int[][]{
+                {0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0},
+                {0, 1, 0, 0, 0, 0},
+                {0, 0, 1, 1, 1, 0},
+                {1, 0, 0, 0, 0, 0}
+        };
+        int[] src = new int[]{4, 2};
+        int[] dst = new int[]{2, 2};
+        int output = robotPath(room, src, dst);
+        System.out.println(output); // --> 8
+    }
+    public static int robotPath(int[][] room, int[] src, int[] dst) {
+        room = aux(room.length, room[0].length, src, 1, room);
+        return room[dst[0]][dst[1]] - 1;
+    }
+
+    public static int[][] aux(int M, int N, int[] candi, int step, int[][] room) {
+        int row = candi[0];
+        int col = candi[1];
+
+        if (row < 0 || row >= M || col < 0 || col >= N) return room;
+
+        if (room[row][col] == 0 || room[row][col] > step) {
+            room[row][col] = step;
+        } else {
+            return room;
+        }
+
+        aux(M, N, new int[]{row + 1, col}, step + 1, room); // 상
+        aux(M, N, new int[]{row - 1, col}, step + 1, room); // 하
+        aux(M, N, new int[]{row, col - 1}, step + 1, room); // 좌
+        aux(M, N, new int[]{row, col + 1}, step + 1, room); // 우
+
+        return room;
+    }
+}
---
+++ codsDaily/src/daily34_X.java
@@ -0,0 +1,42 @@
+public class daily34_X {
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+        int K = 1;
+
+        System.out.println(matrix[0][0]); // --> 1
+        System.out.println(matrix[3][2]); // --> 15
+
+        int[][] rotatedMatrix = rotateMatrix(matrix, K);
+        System.out.println(rotatedMatrix[0][0]); // --> 13
+        System.out.println(rotatedMatrix[3][2]); // --> 8
+    }
+
+    public static int[][] rotateMatrix(int[][] matrix, int K) {
+        if(matrix == null || matrix.length == 0) return matrix;
+
+        int N = matrix.length;
+        int M = matrix[0].length;
+
+        int rotateNum = K % 4;
+        if(rotateNum == 0) return matrix;
+        int[][] rotated = rotateNum % 2 == 1 ? new int[M][N] : new int[N][M];
+
+        for(int row = 0; row < rotated.length; row++) {
+            for(int col = 0; col < rotated[row].length; col++) {
+                if(rotateNum == 1) {
+                    rotated[row][col] = matrix[N - col - 1][row];
+                } else if(rotateNum == 2) {
+                    rotated[row][col] = matrix[N - row - 1][M - col - 1];
+                } else {
+                    rotated[row][col] = matrix[col][M - row - 1];
+                }
+            }
+        }
+        return rotated;
+    }
+}
